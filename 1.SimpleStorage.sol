b327b77a74fb7b3ce3293af0220aaf07d678bcde
Jagadeeshkumar748 <62183612+Jagadeeshkumar748@users.noreply.github.com>
2022-09-27 18:19:04 +0530

    Create 1.SimpleStorage.sol

---
+++ 1.SimpleStorage.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.8;
+
+contract SimpleStorage{
+    uint256  favoriteNumber;
+
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    
+    mapping(string=>uint256) public nameToFavoriteNumber;
+
+    // People public person = People({favoriteNumber:1,name:"krishna"});
+
+    function store(uint256 _favoriteNumber) public{
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrive() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    function getSum(uint number1, uint number2) public pure returns(uint) {
+    uint sum = number1 + number2;
+    return sum;
+    }
+
+    function addPerson(uint256 _favoriteNumber,string memory _name) public{
+        // People memory person = People({favoriteNumber:_favoriteNumber,name:_name});
+        
+        people.push(People(_favoriteNumber,_name));
+        nameToFavoriteNumber[_name]=_favoriteNumber;
+    }
+}
